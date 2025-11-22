28987c7af41881f2a1618341839c2de2ba08ec59
JuraevSherzod <juraevsherzod9943@gmail.com>
2025-11-22 15:04:13 +0500

    scheme file updated in modules package

---
+++ modules/scheme.py
@@ -0,0 +1,50 @@
+from pydantic import BaseModel, field_validator, model_validator
+from fastapi import HTTPException, status
+from numpy import ndarray, issubdtype, number, array
+
+
+class AdalineGDIn(BaseModel):
+    model_config = {
+        'extra': 'forbid'
+    }
+
+    X: list[list[float]]
+    y: list[int]
+
+    @field_validator('X')
+    def verify_X(cls, X):
+        X = array(X)
+        if X.ndim != 2:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+                detail='A 2D matrix must be entered'
+            )
+        if not issubdtype(X.dtype, number):
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+                detail='There should be no NaN values.'
+            )
+        return X
+
+    @field_validator('y')
+    def verify_y(cls, y):
+        y = array(y)
+        if y.ndim != 1:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+                detail='target values must be a vector'
+            )
+        if not issubdtype(y.dtype, number):
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+                detail='There must be no NaN values.'
+            )
+        return y
+
+    @model_validator(mode='after')
+    def verify_object(self):
+        if self.X.shape[0] != self.y.shape[0]:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+                detail='The sample and target values are not the same size.'
+            )
